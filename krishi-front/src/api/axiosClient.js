910b268e349f169bb3cbfbed99d9149ec3872f39
Arya Shrestha <aryashresthaalpha0@gmail.com>
2026-08-31 16:40:27 +0545

    Backend and Frontend deploy

--- krishi-front/src/api/axiosClient.js
+++ krishi-front/src/api/axiosClient.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:5000/api";
+const BASE_URL = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : "http://127.0.0.1:5000/api";
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
